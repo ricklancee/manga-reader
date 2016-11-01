2ed6642dc14baffbd936ac09c03183dbd3f15dd0
Rick Lancee <rickondraw@gmail.com>
2016-11-01 19:18:05 +0100

    added support for es5
    Now works in safari and firefox

--- es5/main.js
+++ es5/main.js
@@ -1,45 +1,6 @@
 'use strict';
 
 window.addEventListener('load', function (_) {
-  var polyfillsNeeded = [];
-  var waitForWebcomponents = false;
-
-  var loadScripts = function loadScripts(urls, yeyCallback, neyCallback) {
-    var count = urls.length;
-    var errored = false;
-
-    if (urls.length == 0) return yeyCallback();
-
-    urls.forEach(function (url) {
-      var script = document.createElement('script');
-      script.onload = function () {
-        if (errored) return;
-        if (! --count) yeyCallback();
-      };
-      script.onerror = function () {
-        if (errored) return;
-        neyCallback();
-        errored = true;
-      };
-      script.src = url;
-      document.head.insertBefore(script, document.head.firstChild);
-    });
-  };
-
-  if (!('Promise' in window)) {
-    polyfillsNeeded.push('polyfills/promise.js');
-  }
-
-  if (!window.fetch) {
-    polyfillsNeeded.push('polyfills/fetch/fetch.js');
-  }
-
-  if (!('registerElement' in document)) {
-    polyfillsNeeded.push('polyfills/webcomponentsjs/CustomElements.min.js');
-    waitForWebcomponents = true;
-  }
-
-  // Initialize
   loadScripts(polyfillsNeeded, function (_) {
     if (waitForWebcomponents) {
       window.addEventListener('WebComponentsReady', function () {
--- main.js
+++ main.js
@@ -1,43 +1,4 @@
 window.addEventListener('load', _ => {
-  let polyfillsNeeded = [];
-  let waitForWebcomponents = false;
-
-  const loadScripts = (urls, yeyCallback, neyCallback) => {
-    let count = urls.length;
-    let errored = false;
-
-    if (urls.length == 0) return yeyCallback();
-
-    urls.forEach(function(url) {
-      var script = document.createElement('script');
-      script.onload = function() {
-        if (errored) return;
-        if (!--count) yeyCallback();
-      };
-      script.onerror = function() {
-        if (errored) return;
-        neyCallback();
-        errored = true;
-      };
-      script.src = url;
-      document.head.insertBefore(script, document.head.firstChild);
-    });
-  };
-
-  if (!('Promise' in window)) {
-    polyfillsNeeded.push('polyfills/promise.js');
-  }
-
-  if (!window.fetch) {
-    polyfillsNeeded.push('polyfills/fetch/fetch.js');
-  }
-
-  if (!('registerElement' in document)) {
-    polyfillsNeeded.push('polyfills/webcomponentsjs/CustomElements.min.js');
-    waitForWebcomponents = true;
-  }
-
-  // Initialize
   loadScripts(polyfillsNeeded, _ => {
     if (waitForWebcomponents) {
       window.addEventListener('WebComponentsReady', function() {
