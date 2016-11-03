0c605bbf7c797d0c8ee377fcdba2351d241876ed
Rick Lancee <rickondraw@gmail.com>
2016-11-03 10:39:56 +0100

    create a custom `polyfillsLoaded` event
    The main.js file was unneccesary large, now a custom event
    `polyfillsLoaded` is fired that tells the browser when all
    polyfills are loaded.

--- main.js
+++ main.js
@@ -1,37 +1,5 @@
-// Util
-const loadScripts = (urls, succesCB, failCB) => {
-  let count = urls.length;
-  let errored = false;
+'use strict';
 
-  if (urls.length == 0) return succesCB();
-
-  urls.forEach(function(url) {
-    var script = document.createElement('script');
-    script.onload = function() {
-      if (errored) return;
-      if (!--count) succesCB();
-    };
-    script.onerror = function() {
-      if (errored) return;
-      failCB();
-      errored = true;
-    };
-    script.src = url;
-    document.head.insertBefore(script, document.head.firstChild);
-  });
-};
-
-window.addEventListener('load', _ => {
-  loadScripts(window.polyfillsNeeded, _ => {
-    if (window.waitForWebcomponents) {
-      window.addEventListener('WebComponentsReady', function() {
-        document.registerElement('manga-reader', MangaReader);
-      });
-      return;
-    }
-
-    document.registerElement('manga-reader', MangaReader);
-  }, _ => {
-    throw new Error('Failed to load polyfills');
-  });
+window.addEventListener('polyFillsLoaded', () => {
+  document.registerElement('manga-reader', MangaReader);
 });
--- polyfills.js
+++ polyfills.js
@@ -1,8 +1,30 @@
 'use strict';
 
 (() => {
-  window.polyfillsNeeded = [];
-  window.waitForWebcomponents = false;
+  const loadScripts = (urls, succesCB, failCB) => {
+    let count = urls.length;
+    let errored = false;
+
+    if (urls.length == 0) return succesCB();
+
+    urls.forEach(function(url) {
+      var script = document.createElement('script');
+      script.onload = function() {
+        if (errored) return;
+        if (!--count) succesCB();
+      };
+      script.onerror = function() {
+        if (errored) return;
+        failCB();
+        errored = true;
+      };
+      script.src = url;
+      document.head.insertBefore(script, document.head.firstChild);
+    });
+  };
+
+  const polyfillsNeeded = [];
+  let waitForWebcomponents = false;
 
   if (!('Promise' in window)) {
     polyfillsNeeded.push('polyfills/promise.js');
@@ -24,4 +46,25 @@
     _HTMLElement.prototype = HTMLElement.prototype;
     HTMLElement = _HTMLElement;
   }
+
+  const polyFillsLoadedEvent = new CustomEvent('polyFillsLoaded', {
+    bubbles: true
+  });
+
+  // Load the polyfills
+  loadScripts(polyfillsNeeded, () => {
+    if (waitForWebcomponents) {
+      window.addEventListener('WebComponentsReady', () => {
+        requestAnimationFrame(() => {
+          document.dispatchEvent(polyFillsLoadedEvent);
+        });
+      })
+    } else {
+      requestAnimationFrame(() => {
+        document.dispatchEvent(polyFillsLoadedEvent);
+      });
+    }
+  }, () => {
+    throw new Error('Failed to load required polyfills');
+  });
 })();
