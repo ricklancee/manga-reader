e3b1cc5866d8e2ed5dbf680046b86e25c2e83a90
Rick Lancee <rickondraw@gmail.com>
2016-11-05 14:54:21 +0100

    now able to load and go through pages while creating panels

--- panel-creator/panels.js
+++ panel-creator/panels.js
@@ -6,13 +6,21 @@ class Panels {
       this.container = document.querySelector('.plotbox');
       this.page = document.querySelector('.plotbox .image');
       this.svg = document.querySelector('svg');
-      this.plotpoints = document.querySelector('.plotpoints code');
+      this.resultsContainer = document.querySelector('.results');
 
       this.clearButton = document.querySelector('.clear-button');
       this.zoomInButton = document.querySelector('.zoom-in');
       this.zoomOutButton = document.querySelector('.zoom-out');
 
       this.fileUploadButton = document.querySelector('.upload-files');
+
+      this.loadedImages = [];
+      this.currentLoadedImageIndex = null;
+
+      const imageData = window.localStorage.getItem('panels-image-data');
+      if (imageData) {
+        console.log(imageData);
+      }
 
       this.pageObject = {};
       this.panels = [];
@@ -111,6 +119,38 @@ class Panels {
       this.zoomOutButton.addEventListener('click', this._zoomOut.bind(this));
 
       this.fileUploadButton.addEventListener('change', this._handleFileSelect.bind(this));
+
+      window.addEventListener('click', (event) => {
+        const closestContainer = this._closest(event.target, 'result');
+
+        if (closestContainer) {
+          const image = this._getImageByFilename(closestContainer.getAttribute('data-filename'));
+
+          if (image) {
+            const index = this.loadedImages.indexOf(image);
+            this._setImage(index);
+          }
+        }
+      });
+    }
+
+    _closest(el, className) {
+      while (!el.classList.contains(className)) {
+        // Increment the loop to the parent node
+        el = el.parentNode;
+        if (!el || el === document.body) {
+          return null;
+        }
+      }
+
+      return el;
+    }
+
+    _setZoom() {
+      this.page.style.width = (this.originalPageWidth * this.zoom) + 'px';
+
+      this._recalc();
+      this._setSvgDimensionsToImage();
     }
 
     _zoomIn() {
@@ -135,7 +175,7 @@ class Panels {
     }
 
     _clearAllPanels() {
-      this.plotpoints.innerHTML = 'No panels plotted.';
+      // this.plotpoints.innerHTML = 'No panels plotted.';
       this.panels = [];
       this.svg.querySelectorAll('path').forEach(pathEl => {
         pathEl.remove();
@@ -240,39 +280,52 @@ class Panels {
         width:  Math.round((highestX - lowestX) * 100) / 100,
         height: Math.round((highestY - lowestY) * 100) / 100
       }
-
     }
 
     _updatePlotpoints() {
-      this.pageObject.image = this.filename;
-      this.pageObject.panels = this.panels;
+
+      let data;
       if (this.panels.length > 0) {
-        var json = JSON.stringify(this.pageObject, null, 2);
-        this.plotpoints.innerHTML = json;
+        data = JSON.stringify(this.panels, null, 2);
       } else {
-        this.plotpoints.innerHTML = 'No panels plotted.';
+        data = 'No panels plotted.';
       }
+
+      const container = document.querySelector('.result[data-index="'+this.currentLoadedImageIndex+'"] code');
+      container.innerHTML = data;
     }
 
-    _setImage(image) {
+    _setImage(index) {
+      this.currentLoadedImageIndex = index;
+      const image = this.loadedImages[index];
+
+      this._loadImage(image.data);
+      document.querySelector('.result.active').classList.remove('active');
+      document.querySelector('.result[data-index="'+index+'"]').classList.add('active');
+
+      this._setZoom();
+    }
+
+    _loadImage(image) {
       this._clearAllPanels();
 
       this.page.src = image;
 
-      setTimeout(() => {
-        this.page.style.width = '';
-
-        this._recalc();
-        this.originalPageWidth = this.pageDimensions.width;
-        this._setSvgDimensionsToImage();
-      }, 200);
-
+      this.page.style.width = '';
+      this._recalc();
+      this.originalPageWidth = this.pageDimensions.width;
+      this._setSvgDimensionsToImage();
     }
 
     _handleFileSelect(event) {
+      this.resultsContainer.innerHTML = '';
+      this.loadedImages = [];
+
       const files = event.target.files; // FileList object
 
-      // Loop through the FileList and render image files as thumbnails.
+      const promises = [];
+
+      // Loop through the FileList
       for (var i = 0, f; f = files[i]; i++) {
 
         // Only process image files.
@@ -283,16 +336,120 @@ class Panels {
         const reader = new FileReader();
 
         // Closure to capture the file information.
-        reader.onload = ((theFile) => {
-          return (e) => {
-            this._setImage(e.target.result);
-            this.filename = theFile.name;
-          };
-        })(f);
+        const promise = new Promise((resolve, reject) => {
+          reader.onload = ((theFile) => {
+            return (e) => {
+              this.loadedImages.push({
+                filename: theFile.name,
+                data: e.target.result
+              });
+
+              resolve();
+            };
+          })(f);
+        });
+
+        promises.push(promise);
 
         // Read in the image file as a data URL.
         reader.readAsDataURL(f);
       }
+
+      // Wait for all promises to resolve
+      Promise.all(promises).then(() => {
+        console.log('all images were loaded');
+        this.loadedImages = this._sortImage(this.loadedImages);
+        this._loadImageData();
+      });
+    }
+
+    _loadImageData() {
+      this._fillResultsContainer(this.loadedImages);
+      // load first image
+      this._setImage(0);
+    }
+
+    _sortImage(images) {
+      images.sort((a, b) => {
+        const filenameA = parseInt(
+          a.filename.replace(/\.(gif|jpg|jpeg|tiff|png)$/i, ''), 10
+        );
+        const filenameB = parseInt(
+          b.filename.replace(/\.(gif|jpg|jpeg|tiff|png)$/i, ''), 10
+        );
+
+        if (filenameA < filenameB) {
+          return -1;
+        }
+
+        if (filenameA > filenameB) {
+          return 1;
+        }
+        return 0;
+      });
+
+      return images;
+    }
+
+    _getImageByFilename(filename) {
+      const index = this.loadedImages.findIndex(element => {
+        if (element.filename === filename) {
+          return true;
+        }
+        return false;
+      });
+
+      if (index === -1) {
+        return false
+      }
+
+      return this.loadedImages[index];
+    }
+
+    _fillResultsContainer(sorted) {
+      const len = sorted.length;
+      const resultDivTemplate = this._createResultContainer();
+
+
+      for (let i = 0; i < len; i++) {
+        const container = resultDivTemplate.cloneNode(true);
+        const title = container.querySelector('label > span:first-child');
+        title.innerHTML = sorted[i].filename;
+        container.setAttribute('data-filename', sorted[i].filename);
+        container.setAttribute('data-index', i);
+        container.id = 'page-' + sorted[i].filename;
+
+        if (i === 0) {
+          container.classList.add('active');
+        }
+
+        this.resultsContainer.appendChild(container);
+
+      }
+    }
+
+    _createResultContainer() {
+      const div = document.createElement('div');
+      const label = document.createElement('label');
+      const spanTitle = document.createElement('span');
+      const spanButton = document.createElement('span');
+      const pre = document.createElement('pre');
+      const code = document.createElement('code');
+
+      div.classList.add('result');
+      spanTitle.innerHTML = '';
+      spanButton.innerHTML = '';
+
+      label.appendChild(spanTitle);
+      label.appendChild(spanButton);
+      code.innerHTML = 'No panels plotted.';
+
+      pre.appendChild(code);
+
+      div.appendChild(label);
+      div.appendChild(pre);
+
+      return div;
     }
 }
 
